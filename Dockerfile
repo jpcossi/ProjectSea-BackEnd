9fec30878c2eb393762a25ff5b0c7e9291a51253
jpcossi <jppcossi@gmail.com>
2024-10-30 01:31:41 -0300

    fix adding variables

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,13 @@ FROM openjdk:17-jdk-slim
 # Set the working directory in the container
 WORKDIR /app
 
+# Copy the Maven wrapper script and its properties files
+COPY mvnw ./
+COPY .mvn .mvn
+
+# Ensure mvnw is executable
+RUN chmod +x mvnw
+
 # Copy the application's JAR file into the container
 # Ensure you have a 'target' directory where your Spring Boot project builds the JAR
 COPY target/gestao_usuarios.jar app.jar
--- src/main/java/com/sea/gestao_usuarios/GestaoUsuariosApplication.java
+++ src/main/java/com/sea/gestao_usuarios/GestaoUsuariosApplication.java
@@ -3,9 +3,22 @@ package com.sea.gestao_usuarios;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class GestaoUsuariosApplication {
+
+
+	
 	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+        
+		// Configurando variáveis de ambiente
+		System.setProperty("MYSQL_USER", dotenv.get("MYSQL_USER"));
+		System.setProperty("MYSQL_PASSWORD", dotenv.get("MYSQL_PASSWORD"));
+		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+		
 		SpringApplication.run(GestaoUsuariosApplication.class, args);
 	}
 
